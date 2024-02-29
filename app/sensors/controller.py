5e23ee93656d658a19e5daadcc1743f809628f86
pausegurab <pausb18@gmail.com>
2024-02-29 19:55:18 +0100

    canvi de pc

--- app/sensors/controller.py
+++ app/sensors/controller.py
@@ -58,8 +58,7 @@ def create_sensor(sensor: schemas.SensorCreate, db: Session = Depends(get_db), m
     db_sensor = repository.get_sensor_by_name(db, sensor.name)
     if db_sensor:
         raise HTTPException(status_code=400, detail="Sensor with same name already registered")
-    raise HTTPException(status_code=404, detail="Not implemented")
-#    return repository.create_sensor(db=db, sensor=sensor)
+    return repository.create_sensor(db=db, sensor=sensor, mongo_db=mongodb_client)
 
 # 🙋🏽‍♀️ Add here the route to get a sensor by id
 @router.get("/{sensor_id}")
@@ -82,11 +81,16 @@ def delete_sensor(sensor_id: int, db: Session = Depends(get_db), mongodb_client:
 # 🙋🏽‍♀️ Add here the route to update a sensor
 @router.post("/{sensor_id}/data")
 def record_data(sensor_id: int, data: schemas.SensorData,db: Session = Depends(get_db) ,redis_client: RedisClient = Depends(get_redis_client)):
-    raise HTTPException(status_code=404, detail="Not implemented")
-    #return repository.record_data(redis=redis_client, sensor_id=sensor_id, data=data)
+    db_sensor = repository.get_sensor(db, sensor_id) 
+    if db_sensor is None:
+        raise HTTPException(status_code=404, detail="Sensor not found")
+    return repository.record_data(sensor_id=sensor_id, db=db, redis=redis_client, data=data)
+
 
 # 🙋🏽‍♀️ Add here the route to get data from a sensor
 @router.get("/{sensor_id}/data")
-def get_data(sensor_id: int, data: schemas.SensorData, db: Session = Depends(get_db) ,redis_client: RedisClient = Depends(get_redis_client)):    
-    raise HTTPException(status_code=404, detail="Not implemented")
-    #return repository.get_data(redis=redis_client, sensor_id=sensor_id, data=data)
+def get_data(sensor_id: int, db: Session = Depends(get_db) ,redis_client: RedisClient = Depends(get_redis_client)):    
+    db_sensor = repository.get_sensor(db, sensor_id) 
+    if db_sensor is None:
+        raise HTTPException(status_code=404, detail="Sensor not found")
+    return repository.get_data(db=db, sensor_id=sensor_id, redis=redis_client)
--- app/sensors/repository.py
+++ app/sensors/repository.py
@@ -1,3 +1,5 @@
+from app.mongodb_client import MongoDBClient
+from app.redis_client import RedisClient
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from typing import List, Optional
@@ -13,20 +15,96 @@ def get_sensor_by_name(db: Session, name: str) -> Optional[models.Sensor]:
 def get_sensors(db: Session, skip: int = 0, limit: int = 100) -> List[models.Sensor]:
     return db.query(models.Sensor).offset(skip).limit(limit).all()
 
-def create_sensor(db: Session, sensor: schemas.SensorCreate) -> models.Sensor:
-    db_sensor = models.Sensor(name=sensor.name, latitude=sensor.latitude, longitude=sensor.longitude)
+def create_sensor(db: Session, sensor: schemas.SensorCreate, mongo_db = MongoDBClient) -> models.Sensor:
+    db_sensor = models.Sensor(name=sensor.name)
     db.add(db_sensor)
     db.commit()
     db.refresh(db_sensor)
+    add_collection(mongo_db=mongo_db, sensor = sensor)
     return db_sensor
 
-def record_data(redis: Session, sensor_id: int, data: schemas.SensorData) -> schemas.Sensor:
-    db_sensordata = data
-    return db_sensordata
+def add_collection(mongo_db: MongoDBClient, sensor: schemas.SensorCreate):
+    info = mongo_db.getCollection("sensors")
+    coll = {
+        "name": sensor.name,
+        "longitude": sensor.longitude,
+        "latitude": sensor.latitude,
+        "type": sensor.type,
+        "mac_address": sensor.mac_address,
+        "manufacturer": sensor.manufacturer,
+        "model" : sensor.model,
+        "serie_number" : sensor.serie_number,
+        "firmware_version" : sensor.firmware_version
+    }
+    info.insert_one(coll)
+    
 
-def get_data(redis: Session, sensor_id: int, data: schemas.SensorData) -> schemas.Sensor:
-    db_sensordata = data
-    return db_sensordata
+def record_data(sensor_id: int, db: Session, redis: RedisClient, data: schemas.SensorData) -> schemas.Sensor:
+    # ldata = last_data.SensorData(temperature=data.temperature, humidity=data.humidity, battery_level=data.battery_level, last_seen=data.last_seen)
+    
+    # Creem les claus compostes per cada un dels atributs
+    temp = "sensor" + str(id) + ":temperatura"
+    hum = "sensor" + str(id) + ":humidity"
+    bat = "sensor" + str(id) + ":battery_level"
+    seen = "sensor" + str(id) + ":last_seen"
+    vel = "sensor" +str(id) +":velocity"
+
+    # Fem els post de cada un dels atributs amb la seva clau i el seu valor
+    redis.set(temp, data.temperature)
+    redis.set(hum, data.humidity)
+    redis.set(bat, data.battery_level)
+    redis.set(seen, data.last_seen)
+    redis.set(vel, data.velocity)
+
+    
+    db_sensor = db.query(models.Sensor).filter(models.Sensor.id == sensor_id).first()
+
+
+    
+
+    return schemas.Sensor(
+        id=db_sensor.id,
+        name=db_sensor.name,
+        latitude=db_sensor.latitude,
+        longitude=db_sensor.longitude,
+        joined_at=str(db_sensor.joined_at),
+        last_seen=data.last_seen,
+        type=var,
+        mac_adress=var,
+        battery_level=data.battery_level,
+        temperature=data.temperature,
+        humidity=data.humidity,
+        velocity=data.velocity
+        
+    )
+
+def get_data(db: Session, sensor_id: int, redis: RedisClient) -> schemas.Sensor:
+    # Trobem el sensor corresponent a la id
+    db_sensor = db.query(models.Sensor).filter(models.Sensor.id == sensor_id).first()
+    
+    # Creem les claus compostes per cada un dels atributs
+    temp = "sensor" + str(id) + ":temperatura"
+    hum = "sensor" + str(id) + ":humidity"
+    bat = "sensor" + str(id) + ":battery_level"
+    seen = "sensor" + str(id) + ":last_seen"
+    vel = "sensor" + str(id) +":velocity"
+
+    # Fem el return amb les dades corresponents del postgres i el redis
+    return schemas.Sensor(
+        id=db_sensor.id,
+        name=db_sensor.name,
+        latitude=db_sensor.latitude,
+        longitude=db_sensor.longitude,
+        joined_at=str(db_sensor.joined_at),
+        last_seen=redis.get(seen),
+        type=var,
+        mac_adress=var,
+        battery_level=redis.get(bat),
+        temperature=redis.get(temp),
+        humidity=redis.get(hum),
+        velocity=redis.get(vel)
+        
+    )
 
 def delete_sensor(db: Session, sensor_id: int):
     db_sensor = db.query(models.Sensor).filter(models.Sensor.id == sensor_id).first()
